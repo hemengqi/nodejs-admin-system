dc259296727b2b2d5b4336ab88c63443a170949a
hemengqi <hemengqi20004@163.com>
2016-04-26 17:05:59 +0800

    mongoDB增加访问权限

--- database/database.js
+++ database/database.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router =  express.Router();
 var mongoose = require('mongoose');
 //测试连接数据库
-mongoose.connect('mongodb://localhost/chihuo',function(e){
-	if(e) mongoose.open = e.message;
-	else mongoose.open = 'mongoose opened';
-});
+mongoose.connect('mongodb://monkey:Monkey@2016@localhost:27017/chihuo');
 var db = mongoose.connection;
+db.on('error',function(){
+	console.error.bind(console,'connection error:');
+	mongoose.open = 'mongoDB not open';
+});
+db.once('open',function(callback){
+	mongoose.open = 'mongoDB opened';
+});
 var userSchema = new mongoose.Schema({
 	name: String,
 	password: String,
@@ -20,10 +24,13 @@ router.get('/insert',function(req,res){
 	console.log('insert');
 	var user = new userModel({name:'dsfdsf',password: 'dsfdsfdsfsfd'});
 	user.save(function(e,product,numberAffected){
-		if(e) res.send(e.message);
-		var html = '<p>新增的数据为：'+ JSON.stringify(product) +'</p>';
-		html += '<p>影响的数据量为：'+numberAffected+'</p>';
-		res.send(html);
+		if(e) {
+			res.send(e.message);
+		}else{
+			var html = '<p>新增的数据为：'+ JSON.stringify(product) +'</p>';
+			html += '<p>影响的数据量为：'+numberAffected+'</p>';
+			res.send(html);
+		}
 	});
 });
 //删
@@ -31,8 +38,11 @@ router.get('/delete',function(req,res){
 	userModel.remove({
 		name: 'dsfdsf'
 	},function(e){
-		if(e) res.send(e.message);
-		res.send('删除成功');
+		if(e) {
+			res.send(e.message);
+		}else{
+			res.send('删除成功');
+		}
 	})
 });
 //改
@@ -43,10 +53,13 @@ router.get('/update',function(req,res){
 	},{
 		password: '435435435'
 	},function(e,raw){
-		if(e) res.send(e.message);
-		var html = '<p>修改的结果为：'+JSON.stringify(raw)+'</p>';
-		//html += '<p>影响的数据量为：'+numberAffected+'</p>';
-		res.send(html);
+		if(e) {
+			res.send(e.message);
+		}else{
+			var html = '<p>修改的结果为：'+JSON.stringify(raw)+'</p>';
+			//html += '<p>影响的数据量为：'+numberAffected+'</p>';
+			res.send(html);
+		}
 	});
 });
 //查
@@ -56,9 +69,12 @@ router.get('/find',function(req,res){
 		name: 'hemengqi'
 	},function(e,docs){
 		console.log(docs);
-		if(e) res.send(e.message);
-		var html = '<p>查询到的数据为：'+JSON.stringify(docs)+'</p>';
-		res.send(html);
+		if(e) {
+			res.send(e.message);
+		}else{
+			var html = '<p>查询到的数据为：'+JSON.stringify(docs)+'</p>';
+			res.send(html);
+		}
 	})
 });
 exports.router = router;
--- routes/angular_demo.js
+++ routes/angular_demo.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
 router.get('/',function(req,res){
-	res.redirect('/demo1');
+	res.redirect('/angular/demo1');
 });
 router.get('/demo1',function(req,res){
 	res.render('angular_demo1',{title:'angular demo 1'});
